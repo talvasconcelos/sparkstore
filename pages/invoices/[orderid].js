b3f3db6b2ee574394e6fcf86993d9d2cb73850ed
Tiago Vasconcelos <tvasconcelos@gmail.com>
2020-11-13 15:45:54 +0000

    getstaticprops working

--- pages/invoices/[orderid].js
+++ pages/invoices/[orderid].js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation } from "@apollo/client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import QRCode from "qrcode";
 import { useToasts } from "react-toast-notifications";
 import { initializeApollo } from "../../lib/apolloClient";
@@ -62,21 +62,15 @@ const InvoicePaid = ({ sats }) => {
 };
 
 const OrderPage = (props) => {
-  // console.log(props);
   const { addToast } = useToasts();
   let showInvoice = false;
   const [qr, setQR] = useState("");
   const [time, setTime] = useState(0);
+  const [cont, setCont] = useState(false);
 
-  const [updateInvoice, { loading: mutationLoading }] = useMutation(
-    UPDATE_INVOICE,
-    {
-      refetchQueries: ["GET_ORDER"],
-      onError(error) {
-        addToast(error.message, { appearance: "error", autoDismiss: true });
-      },
-    }
-  );
+  if (!props.orderid) {
+    return <p>Loading...</p>;
+  }
   const {
     data: { Order } = {},
     loading,
@@ -86,14 +80,18 @@ const OrderPage = (props) => {
   } = useQuery(GET_ORDER, {
     variables: { id: props.orderid },
     pollInterval: 5000,
+    onCompleted(data) {
+      console.log(data);
+      setCont(true);
+    },
   });
 
   if (error) {
-    console.error("Failed to load product", error);
+    console.error("Failed to load order", error);
     addToast(error.message, { appearance: "error", autoDismiss: true });
   }
   if (loading) {
-    return null;
+    return <p>Loading...</p>;
   } else {
     if (Order.status === "expired") {
       stopPolling();
@@ -107,110 +105,128 @@ const OrderPage = (props) => {
   }
   const amount = Order.invoice.amount;
 
+  const [updateInvoice, { loading: mutationLoading }] = useMutation(
+    UPDATE_INVOICE,
+    {
+      refetchQueries: ["GET_ORDER"],
+      onError(error) {
+        addToast(error.message, { appearance: "error", autoDismiss: true });
+      },
+    }
+  );
+
   return (
-    <div className="container grid-lg">
-      <ul class="breadcrumb">
-        <li class="breadcrumb-item">
-          <a href="/">Home</a>
-        </li>
-        <li class="breadcrumb-item">Invoice</li>
-      </ul>
-      <div className="columns">
-        <div className="column col-6 col-sm-12 col-mx-auto">
-          <div class="card">
-            <div class="card-header">
-              <div class="card-title h5">Invoice for your order</div>
-              <div class="card-subtitle text-gray">{amount} sats</div>
-            </div>
-            <div class="card-image">
-              {showInvoice ? (
-                <a href={`lightning:${Order.invoice.payment_request}`}>
-                  <img
-                    src={qr}
-                    alt={Order.invoice.payment_request}
-                    class="img-responsive p-centered"
+    cont && (
+      <div className="container grid-lg">
+        <ul class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="/">Home</a>
+          </li>
+          <li class="breadcrumb-item">Invoice</li>
+        </ul>
+        <div className="columns">
+          <div className="column col-6 col-sm-12 col-mx-auto">
+            <div class="card">
+              <div class="card-header">
+                <div class="card-title h5">Invoice for your order</div>
+                <div class="card-subtitle text-gray">{amount} sats</div>
+              </div>
+              <div class="card-image">
+                {showInvoice ? (
+                  <a href={`lightning:${Order.invoice.payment_request}`}>
+                    <img
+                      src={qr}
+                      alt={Order.invoice.payment_request}
+                      class="img-responsive p-centered"
+                    />
+                  </a>
+                ) : Order.settled ? (
+                  <InvoicePaid sats={amount} />
+                ) : (
+                  <RenewInvoice
+                    updateInvoice={updateInvoice}
+                    invoiceId={Order.invoice.id}
+                    loading={mutationLoading}
                   />
-                </a>
-              ) : Order.settled ? (
-                <InvoicePaid sats={amount} />
-              ) : (
-                <RenewInvoice
-                  updateInvoice={updateInvoice}
-                  invoiceId={Order.invoice.id}
-                  loading={mutationLoading}
-                />
-              )}
-            </div>
-            <div class="card-body">
-              <p>{`Order #: ${props.orderid}`}</p>
-              <ul>
-                {Order.cart.map((item) => (
-                  <li>
-                    {`${item.qty}x ${item.item.title}`}
-                    <span class="text-right float-right">
-                      &euro;{item.item.price * item.qty}
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div class="card-footer">
-              <p class="has-text-weight-semibold">
-                {`Total:`}
-                <span class="text-right float-right">&euro;{Order.total}</span>
-              </p>
-              <span class="text-right float-right">{amount} sats</span>
+                )}
+              </div>
+              <div class="card-body">
+                <p>{`Order #: ${props.orderid}`}</p>
+                <ul>
+                  {Order.cart.map((item) => (
+                    <li>
+                      {`${item.qty}x ${item.item.title}`}
+                      <span class="text-right float-right">
+                        &euro;{item.item.price * item.qty}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div class="card-footer">
+                <p class="has-text-weight-semibold">
+                  {`Total:`}
+                  <span class="text-right float-right">
+                    &euro;{Order.total}
+                  </span>
+                </p>
+                <span class="text-right float-right">{amount} sats</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
-// export async function getServerSideProps({ params }) {
-// const apolloClient = initializeApollo();
-// const _data = await apolloClient.watchQuery({
-//   query: GET_ORDER,
-//   variables: { id: params.orderid },
-//   pollInterval: 5000,
-// });
-// const data = await _data.result();
-// console.log(data);
-// Pass data to the page via props
-// return {
-//   props: {
-//     orderid: params.orderid,
-// Order: data.data.Order,
-// loading: data.loading,
-// error: !data.error ? null : data.error,
-// Observable: _data,
-// startPolling,
-// stopPolling: _data.stopPolling,
-//     },
-//   };
-// }
+export async function getStaticPaths() {
+  return { paths: [{ params: { orderid: "false" } }], fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(
+    `https://sparkstore-backend.herokuapp.com/custom/invoices/${params.orderid}`,
+    {
+      method: "GET",
+    }
+  );
+  const { orderid } = await res.json();
+  // const request = async (id) => {
+  //   const r = await fetch(
+  //     `https://sparkstore-backend.herokuapp.com/custom/invoices/${id}`,
+  //     {
+  //       method: "GET",
+  //     }
+  //   );
+  //   const resp = await r.json();
+  //   console.log(resp);
+  //   return resp;
+  // };
+  // let orderid = await request(params.orderid);
+  return { props: { orderid } };
+}
 
 // OrderPage.getInitialProps = ({ query: { orderid } }) => ({ orderid });
-OrderPage.getInitialProps = async ({ query, req, res }) => {
-  if (req || res) {
-    console.log("server", query.orderid);
-
-    const request = async (id) => {
-      const r = await fetch(
-        `https://sparkstore-backend.herokuapp.com/custom/invoices/${id}`,
-        {
-          method: "GET",
-        }
-      );
-      const resp = await r.json();
-      console.log(resp);
-      return resp;
-    };
-    return await request(req.url.split("/")[2]);
-  }
-  // console.log(ctx.req.url.split("/")[2]);
-  return { orderid: query.orderid };
-};
+// OrderPage.getInitialProps = async ({ query, req, res }) => {
+//   if (req || res) {
+//     console.log("server", query.orderid);
+//
+//     const request = async (id) => {
+//       const r = await fetch(
+//         `https://sparkstore-backend.herokuapp.com/custom/invoices/${id}`,
+//         {
+//           method: "GET",
+//         }
+//       );
+//       const resp = await r.json();
+//       console.log(resp);
+//       return resp;
+//     };
+//     return await request(req.url.split("/")[2]);
+//   }
+//   // console.log(ctx.req.url.split("/")[2]);
+//   return { orderid: query.orderid };
+// };
 
 export default OrderPage;
